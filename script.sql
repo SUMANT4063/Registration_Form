e552aee202ca607e6ee37c72e19b6c64894aee42
T SUMAN <mailtosumant55@gmail.com>
2025-11-21 13:17:02 +0530

    script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,25 @@
+create table Registration_form(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+  fname VARCHAR(50),
+  lname VARCHAR(50),
+  email VARCHAR(100),
+  phone VARCHAR(15),
+  dob DATE,
+  gender VARCHAR(10),
+  qualification VARCHAR(50),
+  branch VARCHAR(50),
+  fatherName VARCHAR(100),
+  fatherPhone VARCHAR(15),
+  motherName VARCHAR(100),
+  motherPhone VARCHAR(15),
+  address TEXT,
+  district VARCHAR(50),
+  istate VARCHAR(50),
+  country VARCHAR(50),
+  material VARCHAR(20),
+  siblings VARCHAR(100),
+  wife VARCHAR(100),
+  children VARCHAR(100)
+)
 
